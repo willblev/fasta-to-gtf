ca26230ae0d472c09f6b7e8b7011af613da332b9
Will <willblev@gmail.com>
2015-11-03 15:56:38 +0100

    improved argument parsing and messages

--- src/fasta-to-gtf.py
+++ src/fasta-to-gtf.py
@@ -1,97 +1,120 @@
 import subprocess, getopt, sys
-import CGAT.Experiment as E
-import CGAT.IOTools as IOTools
-import CGAT.Blat as Blat
-import CGAT.GTF as GTF
+#import CGAT.Experiment as E
+#import CGAT.IOTools as IOTools
+#import CGAT.Blat as Blat
+#import CGAT.GTF as GTF
 
+	
 def usage():
-	print("This is the way you are supposed to use the program")
-	print("Here is an example of the syntax")
-	print("These are the requirements for this program")
-	print("These are the contact details of the coder")
+	print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~fasta-to-gtf~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	print('fasta-to-gtf.py -g <path/reference_genome.fasta> -a <path/assembly.fasta> -o <path/output_name.gtf>')
+	print('This is a python script which produces GTF annotations using a transcriptome assembly and reference genome (in fasta format).')
+	print('The script requires python 2.7 (or later) and the following python modules:')
+	print('getopt, CGAT')
+	print('To run this script, you must specify a reference genome, an assembly, and an output file name.')
+	print('Use -v or --verbose to print intermediate status updates')
+
 
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hoag:v", ["help", "output="])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print(err) # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
-    output = None
-    verbose = False
-    for option, value in opts:
-        if option == in ("-v", "--verbose"):
-            verbose = True
-        elif option in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif option in ("-o", "--output"):
-            output = value
-        elif option in ("-g", "--genome"):
-		   reference_genome = valueel
+	verbose=False
+	assembly=None
+	output=None
+	genome=None
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "o:g:a:hv", ["help", "verbose", "output=", "genome=", "assembly="])
+	except:
+		print 'Unable to parse arguments.'
+		usage()
+		sys.exit()
+
+	for option, value in opts:
+		if option in ("-h", "--help"):
+			usage()
+			sys.exit()
+		elif option in ("-o", "--output"):
+			output = value
+			print output
+		elif option in ("-g", "--genome"):
+			genome = value
+			print genome
 		elif option in ("-a", "--assembly"):
-		reference_genome = value
-        else:
-            assert False, "unrecognized option: known arguments are -v,-h,-o,-g,-a"
-    # ...
+			assembly = value
+			print assembly
+		elif option in ("-v", "--verbose"):
+			verbose = True
+		else:
+			assert False, 'Unrecognized option: known options are -v,-h,-o,-g,-a'
+	if output == None or genome==None or assembly==None:
+		print('All three fields -g -a -o must be specified...')
+		sys.exit()
+
+	blat_command = "blat %s %s -t dna -q dna -tileSize 11 -minIdentity 95 -maxIntron 1001 -out psl" % (genome, assembly)
+	if verbose: 
+		print("Starting BLAT using transcripts from %s as queries to map to the reference genome %s ..." % (assembly,genome))
+		print("Running "+blat_command)
+
+	#blat_output_psl = subprocess.check_output([blat_command, '-1'])
+
+	if verbose:
+		print 'BLAT finished; parsing output and converting to GTF...'
+
+	
+	"""
+	iterator = Blat.BlatIterator(blat_output_psl)
+
+	ninput, noutput, nskipped = 0, 0, 0
+
+	gff = GTF.Entry()
+	gff.source = "psl"
+	gff.feature = "exon"
+
+	ids = {}
+
+	while 1:
+
+		if options.test and ninput >= options.test:
+			break
+
+		match = iterator.next()
+
+		if match is None:
+			break
+
+		ninput += 1
+
+		if match.mQueryId not in ids:
+			ids[match.mQueryId] = 1
+			id = match.mQueryId
+		else:
+			id = match.mQueryId + ":%i" % ids[match.mQueryId]
+			ids[match.mQueryId] += 1
+
+
+		gff.contig = match.mSbjctId
+		gff.gene_id = id
+		gff.transcript_id = id
+
+
+		if id in map_id2strand:
+			gff.strand = map_id2strand[id]
+		else:
+			gff.strand = match.strand
+
+		for qstart, sstart, size in match.getBlocks():
+
+			gff.start = sstart
+			gff.end = sstart + size
+			options.stdout.write(str(gff) + "\n")
+
+		noutput += 1
+		"""
+
+
+
+
 
 if __name__ == "__main__":
-    main()
-
-
-
-blat_output_psl = subprocess.check_output(['blat -t dna -q dna -tileSize 11 -minIdentity 95 -maxIntron 1001 -out psl', '-1'])
-print 'Finished BLAT'
-
-
-iterator = Blat.BlatIterator(blat_output_psl)
-
-ninput, noutput, nskipped = 0, 0, 0
-
-gff = GTF.Entry()
-gff.source = "psl"
-gff.feature = "exon"
-
-ids = {}
-
-while 1:
-
-	if options.test and ninput >= options.test:
-		break
-
-	match = iterator.next()
-
-	if match is None:
-		break
-
-	ninput += 1
-
-	if match.mQueryId not in ids:
-		ids[match.mQueryId] = 1
-		id = match.mQueryId
-	else:
-		id = match.mQueryId + ":%i" % ids[match.mQueryId]
-		ids[match.mQueryId] += 1
-
-
-	gff.contig = match.mSbjctId
-	gff.gene_id = id
-	gff.transcript_id = id
-
-
-	if id in map_id2strand:
-		gff.strand = map_id2strand[id]
-	else:
-		gff.strand = match.strand
-
-	for qstart, sstart, size in match.getBlocks():
-
-		gff.start = sstart
-		gff.end = sstart + size
-		options.stdout.write(str(gff) + "\n")
-
-	noutput += 1
-
+	main()
+    
 
 
